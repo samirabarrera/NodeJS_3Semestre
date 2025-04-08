c5ba2f0407e6c06b9b05b4a8f013b5e0bb4dc11e
samirabarrera <samira.barrera@galileo.edu>
2025-04-07 20:50:01 -0600

    fixing crud in node js

--- Middleware_NodeJS/middleware_nodejs.js
+++ Middleware_NodeJS/middleware_nodejs.js
@@ -1,25 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser'
-import { registrarTurnos, atenderTurnosById, aplicarValidaciones, aplicarValidacionesById, borrarTurno } from './queryInfo';
+//la línea de abajo sive para poder conectar el servidor de node con html
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+import { registrarTurnos, atenderTurnosById, aplicarValidaciones, aplicarValidacionesById, borrarTurno } from './queryInfo';
 const app = express();
 const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running port ${port}`);
+})
 
-app.use (bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: true} ));
 
-app.get('/', (req, res ) => {
-    console.log("Estoy en el endpoint raiz")
-    res.json({ message: "Welcome to my first API"});
-});
+app.get('/', (req, res ) => res.sendFile (__dirname + "/index.html"));
+app.use(express.json());
 
 app.post('/turnos/: nombre, edad, tipo', registrarTurnos);
 app.get('/turnos/: tipoSolicitud', atenderTurnosById);
 app.post('/turnos', aplicarValidaciones)
-app.post('/turnos/: tipoSolicitud', aplicarValidacionesById)
+app.put('/turnos/: tipoSolicitud', aplicarValidacionesById)
 app.delete('/turnos/: alreadyAssisted', borrarTurno);
-
-
-app.listen(port, () => {
-    console.log(`Server is running port ${port}`);
-})
