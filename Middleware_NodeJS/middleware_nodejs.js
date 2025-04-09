b47000178fbb69281d4ce1f8551037f3847398a1
samirabarrera <samira.barrera@galileo.edu>
2025-04-08 19:18:32 -0600

    deleting html and creating final project

--- Middleware_NodeJS/middleware_nodejs.js
+++ Middleware_NodeJS/middleware_nodejs.js
@@ -1,19 +1,11 @@
 import express from 'express';
-//la línea de abajo sive para poder conectar el servidor de node con html
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 import { registrarTurnos, atenderTurnosById, aplicarValidaciones, aplicarValidacionesById, borrarTurno } from './queryInfo';
+
 const app = express();
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running port ${port}`);
 })
-
-
-app.get('/', (req, res ) => res.sendFile (__dirname + "/index.html"));
-app.use(express.json());
 
 app.post('/turnos/: nombre, edad, tipo', registrarTurnos);
 app.get('/turnos/: tipoSolicitud', atenderTurnosById);
